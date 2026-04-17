cfa85de4cf047753f147bb80e462e26964a962b5
Carreon-9E-ay2526 <carreon.jo@obmontessori.edu.ph>
2026-04-17 10:27:20 +0800

    Added the code

---
+++ main.js
@@ -0,0 +1,10 @@
+function letCarsin(){
+
+let carCounter = 0;
+const maxCars = 1000000;
+
+while (carCounter < maxCars) { window.alert("Car number " + carCounter + " is entering");
+    carCounter +=100000;
+    }
+window.alert("Full")
+}
